2c3468f03de24808e778f37b4ef01a4b3014035d
nekopink <saruwan1509@gmail.com>
2025-03-26 13:25:57 +0900

    Update ownerConfig.js

--- media/settings/ownerConfig.js
+++ media/settings/ownerConfig.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 // gausah di ubah rek karna
 // udh ada request pas di scan pairing
 global.ownername = 'xZiyy';
-global.owner = '6283804078729';
+global.owner = '60129143872';
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
